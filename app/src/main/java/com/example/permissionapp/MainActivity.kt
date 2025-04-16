09e8df5a5129e18a5bd2aa179a3912a0ffb322d0
atkmehmet <durmazmehmet47@gmail.com>
2025-04-16 23:51:56 +0300

    datetimepicker

--- app/src/main/java/com/example/permissionapp/MainActivity.kt
+++ app/src/main/java/com/example/permissionapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
               // PermissionScreen()
                 //Screen()
               //mainScreen()
-                DatePickerExamples()
+                DatePickerDocked()
             }
         }
     }
--- app/src/main/java/com/example/permissionapp/timePicker.kt
+++ app/src/main/java/com/example/permissionapp/timePicker.kt
@@ -1,206 +1,21 @@
 package com.example.permissionapp
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.waitForUpOrCancellation
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material3.Button
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.DateRangePicker
-import androidx.compose.material3.DisplayMode
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberDatePickerState
-import androidx.compose.material3.rememberDateRangePickerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
-//import com.example.compose.snippets.touchinput.userinteractions.MyAppTheme
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-
-// [START android_compose_components_datepicker_examples]
-// [START_EXCLUDE]
-@Composable
-fun DatePickerExamples() {
-    var showModal by remember { mutableStateOf(false) }
-    var showModalInput by remember { mutableStateOf(false) }
-    var showRangeModal by remember { mutableStateOf(false) }
-// [END_EXCLUDE]
-    var selectedDate by remember { mutableStateOf<Long?>(null) }
-// [START_EXCLUDE]
-    var selectedDateRange by remember { mutableStateOf<Pair<Long?, Long?>>(null to null) }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        Text("Docked date picker:")
-        DatePickerDocked()
-
-        Text("Open modal picker on click")
-        DatePickerFieldToModal()
-
-        Text("Modal date pickers:")
-        Button(onClick = { showModal = true }) {
-            Text("Show Modal Date Picker")
-        }
-        Button(onClick = { showModalInput = true }) {
-            Text("Show Modal Input Date Picker")
-        }
-// [END_EXCLUDE]
-        if (selectedDate != null) {
-            val date = Date(selectedDate!!)
-            val formattedDate = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(date)
-            Text("Selected date: $formattedDate")
-        } else {
-            Text("No date selected")
-        }
-// [START_EXCLUDE]
-
-        Text("Date range pickers:")
-
-        Button(onClick = { showRangeModal = true }) {
-            Text("Show Modal Date Range Picker")
-        }
-
-        if (selectedDateRange.first != null && selectedDateRange.second != null) {
-            val startDate = Date(selectedDateRange.first!!)
-            val endDate = Date(selectedDateRange.second!!)
-            val formattedStartDate = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(startDate)
-            val formattedEndDate = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(endDate)
-            Text("Selected date range: $formattedStartDate - $formattedEndDate")
-        } else {
-            Text("No date range selected")
-        }
-    }
-
-    if (showModal) {
-// [END_EXCLUDE]
-        DatePickerModal(
-            onDateSelected = {
-                selectedDate = it
-                showModal = false
-            },
-            onDismiss = { showModal = false }
-        )
-    }
-// [START_EXCLUDE]
-
-    if (showModalInput) {
-        DatePickerModalInput(
-            onDateSelected = {
-                selectedDate = it
-                showModalInput = false
-            },
-            onDismiss = { showModalInput = false }
-        )
-    }
-
-    if (showRangeModal) {
-        DateRangePickerModal(
-            onDateRangeSelected = {
-                selectedDateRange = it
-                showRangeModal = false
-            },
-            onDismiss = { showRangeModal = false }
-        )
-    }
-}
-// [END android_compose_components_datepicker_examples]
+import java.util.*
 
 @OptIn(ExperimentalMaterial3Api::class)
-// [START android_compose_components_datepicker_modal]
-@Composable
-fun DatePickerModal(
-    onDateSelected: (Long?) -> Unit,
-    onDismiss: () -> Unit
-) {
-    val datePickerState = rememberDatePickerState()
-
-    DatePickerDialog(
-        onDismissRequest = onDismiss,
-        confirmButton = {
-            TextButton(onClick = {
-                onDateSelected(datePickerState.selectedDateMillis)
-                onDismiss()
-            }) {
-                Text("OK")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text("Cancel")
-            }
-        }
-    ) {
-        DatePicker(state = datePickerState)
-    }
-}
-// [END android_compose_components_datepicker_modal]
-
-@OptIn(ExperimentalMaterial3Api::class)
-// [START android_compose_components_datepicker_inputmodal]
-@Composable
-fun DatePickerModalInput(
-    onDateSelected: (Long?) -> Unit,
-    onDismiss: () -> Unit
-) {
-    val datePickerState = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
-
-    DatePickerDialog(
-        onDismissRequest = onDismiss,
-        confirmButton = {
-            TextButton(onClick = {
-                onDateSelected(datePickerState.selectedDateMillis)
-                onDismiss()
-            }) {
-                Text("OK")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text("Cancel")
-            }
-        }
-    ) {
-        DatePicker(state = datePickerState)
-    }
-}
-// [END android_compose_components_datepicker_inputmodal]
-
-@OptIn(ExperimentalMaterial3Api::class)
-// [START android_compose_components_datepicker_docked]
 @Composable
 fun DatePickerDocked() {
     var showDatePicker by remember { mutableStateOf(false) }
@@ -253,94 +68,21 @@ fun DatePickerDocked() {
     }
 }
 
-@Composable
-fun DatePickerFieldToModal(modifier: Modifier = Modifier) {
-    var selectedDate by remember { mutableStateOf<Long?>(null) }
-    var showModal by remember { mutableStateOf(false) }
-
-    OutlinedTextField(
-        value = selectedDate?.let { convertMillisToDate(it) } ?: "",
-        onValueChange = { },
-        label = { Text("DOB") },
-        placeholder = { Text("MM/DD/YYYY") },
-        trailingIcon = {
-            Icon(Icons.Default.DateRange, contentDescription = "Select date")
-        },
-        modifier = modifier
-            .fillMaxWidth()
-            .pointerInput(selectedDate) {
-                awaitEachGesture {
-                    // Modifier.clickable doesn't work for text fields, so we use Modifier.pointerInput
-                    // in the Initial pass to observe events before the text field consumes them
-                    // in the Main pass.
-                    awaitFirstDown(pass = PointerEventPass.Initial)
-                    val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
-                    if (upEvent != null) {
-                        showModal = true
-                    }
-                }
-            }
-    )
-
-    if (showModal) {
-        DatePickerModal(
-            onDateSelected = { selectedDate = it },
-            onDismiss = { showModal = false }
-        )
-    }
-}
-
 fun convertMillisToDate(millis: Long): String {
     val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
     return formatter.format(Date(millis))
 }
-// [END android_compose_components_datepicker_docked]
 
-@OptIn(ExperimentalMaterial3Api::class)
-// [START android_compose_components_datepicker_range]
+@Preview(showBackground = true)
 @Composable
-fun DateRangePickerModal(
-    onDateRangeSelected: (Pair<Long?, Long?>) -> Unit,
-    onDismiss: () -> Unit
-) {
-    val dateRangePickerState = rememberDateRangePickerState()
-
-    DatePickerDialog(
-        onDismissRequest = onDismiss,
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    onDateRangeSelected(
-                        Pair(
-                            dateRangePickerState.selectedStartDateMillis,
-                            dateRangePickerState.selectedEndDateMillis
-                        )
-                    )
-                    onDismiss()
-                }
-            ) {
-                Text("OK")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text("Cancel")
-            }
-        }
+fun DatePickerPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        DateRangePicker(
-            state = dateRangePickerState,
-            title = {
-                Text(
-                    text = "Select date range"
-                )
-            },
-            showModeToggle = false,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(500.dp)
-                .padding(16.dp)
-        )
+        DatePickerDocked()
     }
 }
-// [END android_compose_components_datepicker_range]
