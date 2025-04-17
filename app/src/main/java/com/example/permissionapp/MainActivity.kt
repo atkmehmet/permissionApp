7cad5425c2b1318e049051ee3ffd25084c1d01b0
atkmehmet <durmazmehmet47@gmail.com>
2025-04-17 23:38:47 +0300

    datetimepicker

--- app/src/main/java/com/example/permissionapp/MainActivity.kt
+++ app/src/main/java/com/example/permissionapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
               // PermissionScreen()
                 //Screen()
               //mainScreen()
-                AddDatePicker()
+                DatePickerExamples()
             }
         }
     }
